b7599f2fc966f6066622c4289c4ff4ce495bd4bb
Ayoade <mcolaniyan@gmail.com>
2024-01-03 12:32:41 +0100

    new

--- main.py
+++ main.py
@@ -2,14 +2,18 @@ from kivy.app import App
 from kivy.uix.label import Label
 class MyApp(App):
     def build(self):
-        return Label(text="hello world")
+        #return Label(text="hello world")
+        return Label(text="hello world", font_size='50',color="pink")
 if __name__ == '__main__':
     MyApp().run()
 
 
 
+
+
+
     
 
 
-    
+
 
