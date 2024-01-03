d2ea4c9122d957d0bc5aa3ddb970c5f7400a20d2
Ayoade <mcolaniyan@gmail.com>
2024-01-03 13:15:06 +0100

    new

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ class MyApp(App):
 
 
         layout = FloatLayout()
-        label1 = Label(text='Hello', size_hint = (0.3 ,0.2),pos_hint =('center_x':0.2,'center_y':0.5))
+        label1 = Label(text='Hello', size_hint = (0.3 ,0.2),pos_hint=('center_x':0.2,'center_y':0.5))
         label2 = Label(text = 'Happy New Year',size_hint = (0.5,0.7), pos_hint = ('center_x':0.5,'center_y':0.1))
         layout.add_widget(label1)
         layout.add_widget(label2)
